92dba7dc6277340fc05254e081f74a924c1d721e
Navneetkumarjpr <navaneetjpr@gmail.com>
2021-08-24 18:07:10 +0530

    spaces

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
-
 import "./App.css";
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
 import HomePage from "./Pages/Home/HomePage";
-
 import ProgramsPage from "./Pages/Programs/ProgramsPage";
 import AboutPage from "./Pages/About/AboutPage";
 import ContactPage from "./Pages/Contact/ContactPage";
 import LoginPage from "./Pages/Login/LoginPage";
 import SignupPage from "./Pages/SignUp/SignupPage";
-
 import NotfoundPage from "./Pages/NotfoundPage/NotfoundPage";
-
 function App() {
   return (
     <Router>
       <NavBar />
-      <Switch>
+       <Switch>
         <Route exact path="/" component={HomePage} />
         <Route path="/programs" component={ProgramsPage} />
         <Route path="/about" component={AboutPage} />
@@ -25,8 +21,8 @@ function App() {
         <Route path="/login" component={LoginPage} />
         <Route path="/signup" component={SignupPage} />
         <Route component={NotfoundPage} />
-      </Switch>
-      <Footer />
+       </Switch>
+      <Footer/>
     </Router>
   );
 }
