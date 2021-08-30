5eb62ad00ac2e662ad822713fa11d0d5c975a81e
ShrutiOmer <shrutiomer1234@gmail.com>
2021-08-30 23:09:33 +0530

    blog added

--- src/parts/Blog/BlogSection1/BlogSection1.js
+++ src/parts/Blog/BlogSection1/BlogSection1.js
@@ -6,69 +6,44 @@ import left from "./image/right-arrow (1) 2.png";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch } from '@fortawesome/free-solid-svg-icons';
 import Carousel from 'react-bootstrap/Carousel'
+import blogSlider from '../../../utils/BlogSlides.json'
+import "./BlogSection.css";
+import { Link } from "react-router-dom";
 
+const BlogSection1 = () => {
+  const nextIcon = <span className="glyphicon glyphicon-glass"><img src={right} /></span>;
+  const prevIcon = <span className="glyphicon glyphicon-glass"><img src={left} /></span>;
+  return (
+    <div className={styles.BlogSliderContainer}>
+      <div className={styles.popular}><Link className={styles.plink} to='/'>Popular</Link></div>
+      <Carousel nextIcon={nextIcon} prevIcon={prevIcon} >
+        {
+          blogSlider.map((Slide) => {
+            const path="./image/"+Slide.img
+            return (
+              <Carousel.Item>
+                <img
+                  className="d-block w-100"
+                  style={{ height: "84vh" }}
+                  src={img}
+                  alt="First slide"
+                />
+                <Carousel.Caption style={{ top: "16.25rem" }}>
+                  <h3>{Slide.title}</h3>
+                  <p>{Slide.info}</p>
+                  <div className={styles.searchbar}>
+                    <input type="search" placeholder="Search Category " />
+                    <FontAwesomeIcon className={styles.icon} icon={faSearch} />
+                  </div>
+                </Carousel.Caption>
+              </Carousel.Item>
+            )
+          })
+        }
 
-const BlogSection1=()=>{
-    const nextIcon= <span className="glyphicon glyphicon-glass"><img src={right} /></span>;
-    const prevIcon= <span className="glyphicon glyphicon-glass"><img src={left} /></span>;
-    return(
-        <div className={styles.BlogSliderContainer}>
-            
-            <Carousel nextIcon ={nextIcon} prevIcon={prevIcon} indicatorLabels={[1,2,3]}>
-            
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      style={{height:"84vh"}}
-      src={img}
-      alt="First slide"
-    />
-    <Carousel.Caption style={{top:"16.25rem"}}>
-      <h3>LoremIpsum</h3>
-      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-      <div className={styles.searchbar}>
-      <input type="search" placeholder="Search Category " />
-      <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      style={{height:"84vh"}}
-      src={img}
-      alt="Second slide"
-    />
-
-    <Carousel.Caption style={{top:"16.25rem"}}>
-      <h3>LoremIpsum</h3>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <div className={styles.searchbar}>
-      <input type="search" placeholder="Search Category " />
-      <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-  <Carousel.Item>
-    <img
-      className="d-block w-100"
-      style={{height:"84vh"}}
-      src={img}
-      alt="Third slide"
-    />
-
-    <Carousel.Caption style={{top:"16.25rem"}}>
-      <h3>LoremIpsum</h3>
-      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
-      <div className={styles.searchbar}>
-      <input type="search" placeholder="Search Category " />
-      <FontAwesomeIcon className={styles.icon} icon={faSearch} />
-      </div>
-    </Carousel.Caption>
-  </Carousel.Item>
-</Carousel>
-        </div>
-    );
+      </Carousel>
+    </div>
+  );
 
 };
 
--- src/parts/Blog/BlogSection3/BlogSection3.js
+++ src/parts/Blog/BlogSection3/BlogSection3.js
@@ -1,20 +1,35 @@
-import React from "react";
+import React, { useState } from 'react'
+import BlogCard2 from '../../../components/BlogCard/BlogCard2/BlogCard2'
+import blogCard from '../../../utils/BlogCard.json'
 import styles from "./BlogSection3.module.css";
-import BlogCard from "../../../components/BlogCard/BlogCard"
-// import "./ContactPage.css";
+import BlogCard1 from "../../../components/BlogCard/BlogCard1/BlogCard1"
+import clock from "./image/clock (4) 1.png"
+import trend from "./image/trending 1.png"
 
 const BlogSection3 = () => {
-  return (<div className={styles.BlogBlogContainer}>
-      <div className={styles.blogbox1}>
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
-        <BlogCard />
+  const [toggle,toggleState]=useState('recent');
+  return (
+  <div className={styles.BlogBlogContainer}>
+    <div className={styles.blogbox1}>
+      <BlogCard1 />
+    </div>
+    <div className={styles.blogbox2}>
+      <div class="article">
+        <div class={styles.btnsec}>
+            <button type="button" className={styles.btn1} onClick={() => toggleState('recent')}><img class="clock" src={clock} alt="clk-img"/> Recent Articles</button>
+            <button type="button" className={styles.btn2} onClick={() => toggleState('top')}><img class="trend" src={trend} alt="trend-img"/> Popular Articles</button>
+        </div>
+        {
+          toggle == 'recent' && (blogCard.map((card) => (<BlogCard2 key={blogCard.id} blogData={card} />)))
+        }
+        {
+          toggle == 'top' && (blogCard.map((card) => (<BlogCard2 key={blogCard.id} blogData={card} />)))
+        }
       </div>
-      <div className={styles.blogbox2}></div>
-      <div></div>
+      <div class={styles.morebtn}>
+            <button type="button" class={styles.btn3}>VIEW MORE</button>
+      </div>
+    </div>
   </div>);
 };
 
