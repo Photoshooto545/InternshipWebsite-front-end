54e7074cdadeb48af8bbb7dc8625dcb008330958
Zainabraja <zainabraja53@gmail.com>
2021-09-27 14:39:53 +0530

    Add Skills and Changes in responsiveness

--- src/Pages/Contact/ContactPage.js
+++ src/Pages/Contact/ContactPage.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "./ContactPage.css";
-import SignupPage from "../../SignUp/SignUp";
 
 const ContactPage = () => {
-  // return <div className="contactPageContainer">Contact us Page</div>;
-  return <div className="contactPageContainer"><SignupPage /></div>;
+  return <div className="contactPageContainer">Contact us Page</div>;
 };
 
 export default ContactPage;
--- src/components/BlogCard/BlogCard1/BlogCard1.js
+++ src/components/BlogCard/BlogCard1/BlogCard1.js
@@ -10,32 +10,32 @@ const BlogCard1 = () => {
    return (
       blogCard.map((card) => {
          return (
-               <div className={styles.card}>
-                  <div className={styles.image}><img src={card.img} alt="" /></div>
-                  <div className={styles.status}>
-                     <li className={styles.type}>{card.status.type}</li>
-                     <li className={styles.date}>{card.status.date}</li>
-                     <li className={styles.readtime}>{card.status.readtime}</li>
+            <div className={styles.card}>
+               <div className={styles.image}><img src={card.img} alt="" /></div>
+               <div className={styles.status}>
+                  <li className={styles.type}>{card.status.type}</li>
+                  <li className={styles.date}>{card.status.date}</li>
+                  <li className={styles.readtime}>{card.status.readtime}</li>
+               </div>
+               <div className={styles.totalcontent}>
+                  <h1 className={styles.heading}>{card.title}</h1>
+                  <p className={styles.content}>{card.info} <Link to={card.link}>Read More</Link></p>
+               </div>
+               <div className={styles.data}>
+                  <div className={styles.like}>
+                     <img src={heart} alt="" />
+                     <p className={styles.text}>{card.review.like}</p>
                   </div>
-                  <div className={styles.totalcontent}>
-                     <h1 className={styles.heading}>{card.title}</h1>
-                     <p className={styles.content}>{card.info} <Link to={card.link}>Read More</Link></p>
+                  <div className={styles.comment}>
+                     <img src={comment} alt="" />
+                     <p className={styles.text}>{card.review.comment}</p>
                   </div>
-                  <div className={styles.data}>
-                     <div className={styles.like}>
-                        <img src={heart} alt="" />
-                        <p className={styles.text}>{card.review.like}</p>
-                     </div>
-                     <div className={styles.comment}>
-                        <img src={comment} alt="" />
-                        <p className={styles.text}>{card.review.comment}</p>
-                     </div>
-                     <div className={styles.view}>
-                        <img src={view} alt="" />
-                        <p className={styles.text}>{card.review.views}</p>
-                     </div>
+                  <div className={styles.view}>
+                     <img src={view} alt="" />
+                     <p className={styles.text}>{card.review.views}</p>
                   </div>
                </div>
+            </div>
          )
       })
 
