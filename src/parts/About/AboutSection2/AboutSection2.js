0550d63734ac0dbc514064fee9a775023f391e84
Zainabraja <zainabraja53@gmail.com>
2021-09-28 19:58:17 +0530

    Changes in responsiveness

--- src/parts/About/AboutSection2/AboutSection2.js
+++ src/parts/About/AboutSection2/AboutSection2.js
@@ -4,27 +4,27 @@ import styles from "./AboutSection2.module.css";
 const AboutSection2 = () => {
     return (
         <>
-             <section className={styles.whatwe}>
-        <h1>What We Do</h1>
-    <div className={styles.row}>
-    <div className={styles.coursecol}>
-         
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti expedita illum corrupti a dolorum labore modi eveniet unde, 
-            asperiores provident nesciunt est explicabo totam illo rem perferendis laborum rerum?</p>
-    </div>
-    <div className={styles.coursecol}>
-         
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti expedita illum corrupti a dolorum labore modi eveniet unde, 
-            asperiores provident nesciunt est explicabo totam illo rem perferendis laborum rerum?</p>
-    </div>
-    <div className={styles.coursecol}>
-        
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti expedita illum corrupti a dolorum labore modi eveniet unde, 
-            asperiores provident nesciunt est explicabo totam illo rem perferendis laborum rerum?</p>
-    </div>
-    </div>
+            <section className={styles.whatwe}>
+                <h1>What We Do</h1>
+                <div className={styles.row}>
+                    <div className={styles.coursecol}>
 
-     </section>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti expedita illum corrupti a dolorum labore modi eveniet unde,
+                            asperiores provident nesciunt est explicabo totam illo rem perferendis laborum rerum?</p>
+                    </div>
+                    <div className={styles.coursecol}>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti expedita illum corrupti a dolorum labore modi eveniet unde,
+                            asperiores provident nesciunt est explicabo totam illo rem perferendis laborum rerum?</p>
+                    </div>
+                    <div className={styles.coursecol}>
+
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut deleniti expedita illum corrupti a dolorum labore modi eveniet unde,
+                            asperiores provident nesciunt est explicabo totam illo rem perferendis laborum rerum?</p>
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }
